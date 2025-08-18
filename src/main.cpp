c57fe9598951dea62b2fa9a489c654599efc1725
Lan Phuong Dinh <phuong@haufschild.de>
2025-08-18 14:53:26 +0200

    Update 18.08.2025

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,9 @@
 
 
 
-int noOfSteps = 500;           // Number of steps to move in each direction
-int microSecondsDelay = 1000;  // Delay in microseconds between each step
-void setup() {
+//int noOfSteps = 500;           // Number of steps to move in each direction
+//int microSecondsDelay = 1000;  // Delay in microseconds between each step
+/*void setup() {
  // Configure pin modes
  pinMode(EN_PIN, OUTPUT);
  pinMode(STEP_PIN, OUTPUT);
@@ -38,23 +38,23 @@ void loop() {
  // Move motor in the opposite direction
  digitalWrite(DIR_PIN, HIGH); // Set direction to HIGH
  moveSteps(noOfSteps);
-}
+}*/
 
 
 
-/*AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
-
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+int gotoPos;
 void setup(){
   Serial.begin(115220);
   pinMode(EN_PIN, OUTPUT);
   digitalWrite(EN_PIN, LOW);
 
-  stepper.setMaxSpeed(1000);
-  stepper.setAcceleration(500);
+  stepper.setMaxSpeed(3000);
+  stepper.setAcceleration(1000);
 }
 
 void loop(){
-  if (Serial.available()){
+/* if (Serial.available()){
     String cmd = Serial.readStringUntil('\n');
     cmd.trim();
 
@@ -65,7 +65,42 @@ void loop(){
       }
       Serial.println("DONE");
     }
-  }
-}
-*/
+  }*/
 
+  gotoPos = 5000;
+  stepper.moveTo(gotoPos);
+  stepper.runSpeedToPosition();
+  delay(1000);
+
+  gotoPos = 0;
+  stepper.moveTo(gotoPos);
+  stepper.runSpeedToPosition();
+  delay(1000);
+}
+
+/*void setup() {
+  // Sets the two pins as Outputs
+  pinMode(STEP_PIN,OUTPUT); 
+  pinMode(DIR_PIN,OUTPUT);
+}
+void loop() {
+  digitalWrite(DIR_PIN,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(STEP_PIN,HIGH); 
+    delayMicroseconds(700);    // by changing this time delay between the steps we can change the rotation speed
+    digitalWrite(STEP_PIN,LOW); 
+    delayMicroseconds(700); 
+  }
+  delay(1000); // One second delay
+  
+  digitalWrite(DIR_PIN,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 1600; x++) {
+    digitalWrite(STEP_PIN,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(STEP_PIN,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}*/
